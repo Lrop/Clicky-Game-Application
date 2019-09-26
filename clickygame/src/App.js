ce19be74831299424c7dfa8c9e893acc1b694396
Lrop <Lagropner04@gmail.com>
2019-09-26 17:26:23 -0600

    save

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
-import Card from "./components/Card";
+import Tree from "./components/Tree";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-import cards from "./cards.json";
+import trees from "./trees.json";
 import "./App.css";
 
 class App extends Component {
-  // Setting this.state.cards to the cards json array
+ 
   state = {
-    cards,
+    trees : trees,
     score: 0,
     highscore: 0
-  };
-
+  }
   gameOver = () => {
     if (this.state.score > this.state.highscore) {
       this.setState({highscore: this.state.score}, function() {
@@ -30,8 +29,8 @@ class App extends Component {
   clickCount = id => {
     this.state.cards.find((o, i) => {
       if (o.id === id) {
-        if(cards[i].count === 0){
-          cards[i].count = cards[i].count + 1;
+        if(trees[i].count === 0){
+          trees[i].count = trees[i].count + 1;
           this.setState({score : this.state.score + 1}, function(){
             console.log(this.state.score);
           });
@@ -43,22 +42,27 @@ class App extends Component {
       }
     });
   }
-  // Map over this.state.cards and render a cardCard component for each card object
+
+  
   render() {
     return (
       <Wrapper>
-        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
-        {this.state.cards.map(card => (
-          <Card
-            clickCount={this.clickCount}
-            id={card.id}
-            key={card.id}
-            image={card.image}
-          />
-        ))}
+     <Header score={this.state.score} highscore={this.state.highscore} title={"Trees Memory Game"} message={this.state.message} />
+        <div className="container">
+          {this.state.trees.map((tree) => (
+            <Tree
+              clickcount={this.clickCount}
+              id={tree.id}
+              key={tree.id}
+              image={tree.image}
+              name={tree.name}
+            />
+          ))}
+        </div>
       </Wrapper>
     );
   }
 }
 
 export default App;
+
---
+++ clickygame/src/components/Tree/tree.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./tree.css";
+
+
+const Tree = props => (
+
+    <div className="tree" onClick={() => props.clickCount(props.id)}>
+        <div className="image-container">
+            <img alt={props.name} src={props.image} />
+        </div>
+    </div>
+);
+
+export default Tree;
